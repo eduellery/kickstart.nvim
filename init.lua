98325f6e9881fd2f86de82423aa5890de4b41224
Eduardo Ellery <eduellery@gmail.com>
2026-08-08 15:58:29 +0200

    fix: re-add neo-tree as vim.pack

--- init.lua
+++ init.lua
@@ -461,6 +461,20 @@ require('lualine').setup {
   }
 }
 
+vim.pack.add({
+  {
+    src = 'https://github.com/nvim-neo-tree/neo-tree.nvim',
+    version = vim.version.range('3')
+  },
+  -- dependencies
+  "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/MunifTanjim/nui.nvim",
+  -- optional, but recommended
+  "https://github.com/nvim-tree/nvim-web-devicons",
+})
+
+vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
+
 -- ============================================================
 -- SECTION 5: SEARCH & NAVIGATION
 -- Telescope setup, keymaps, LSP picker mappings
