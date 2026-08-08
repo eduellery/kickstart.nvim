7ce5facfbb38ead125c1b29e2b379bedd41eae14
Eduardo Ellery <eduellery@gmail.com>
2026-08-08 16:21:07 +0200

    chore: remove unused plugins

--- lua/plugins/gruvbox.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000 ,
-    config = function()
-        -- Gruvbox shenanigans
-        vim.o.background = "dark" -- or "light" for light mode
-        vim.cmd.colorscheme "gruvbox"
-    end
-}
